4887785cda5847327631238d18ec4124b16334fa
juliazxu <juliazxu@gmail.com>
2018-07-06 15:04:48 -0700

    Cleaned up components and css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,21 +7,47 @@ const bettaBlue = require("./../assets/img/betta-blue.png");
 export interface HomeProps {
 }
 
+export interface LandingFormProps {
+}
+
+export class LandingForm extends React.Component<LandingFormProps, undefined> {
+  render() {
+    return (
+      <div className="section-black flex-row">
+        <span className="flex-column">
+          <h1 className="white landing-prompt">Everything you need to give your betta the best life possible</h1>
+          <input type="text" id="email" name="email" placeholder="email@mail.com" />
+          <span className="button-container">
+            <input type="button" className="primary-button" value="Send me the best products" />
+          </span>
+        </span>
+        <img className="align-right" src={bettaBlue} />
+      </div>
+    )
+  }
+}
+
+export class SuppliesCards extends React.Component<LandingFormProps, undefined> {
+  render() {
+    return (
+      <div className="section-white flex-row">
+        <span className="flex-row">
+
+        </span>
+        <span className="flex-row">
+
+        </span>
+      </div>
+    )
+  }
+}
+
 export default class Home extends React.Component<HomeProps, undefined> {
   render() {
     return (
       <>
         <Header />
-        <div className="section-black flex-row">
-          <span className="flex-column">
-            <h1 className="white landing-prompt">Everything you need to give your betta the best life possible</h1>
-            <input type="text" id="email" name="email" placeholder="email@mail.com" />
-            <span className="button-container">
-              <input type="button" className="primary-button" value="Email me the best prices" />
-            </span>
-          </span>
-          <img className="align-right" src={bettaBlue} />
-        </div>
+        <LandingForm />
       </>
     );
   }
