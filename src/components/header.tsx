97ac323287b21c9066ca1f76d184874d18a7ea90
juliazxu <juliazxu@gmail.com>
2018-07-08 21:03:06 -0700

    Added media queries

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,20 @@ export interface HeaderProps {
 }
 
 export default class Header extends React.Component<HeaderProps, undefined> {
+  alert() {
+    alert("Coming soon!");
+  }
+
   render() {
     return (
       <div className="header">
         <img src={logo} height="56px"/>
-        <span className="links">Care FAQ - Coming soon</span>
-        <span className="links">Supplies - Coming soon</span>
+        <span className="links" onClick={() => this.alert()}>
+          Care FAQ - Coming soon
+        </span>
+        <span className="links" onClick={() => this.alert()}>
+          Supplies - Coming soon
+        </span>
       </div>
     );
   }
